e6a7cd9fb90146a83e13bf80a43c3d67fde058c6
CBon <cbond74@gmail.com>
2020-06-20 14:40:38 +0300

    3 13 test UserService

--- src/test/java/ru/javawebinar/topjava/web/user/InMemoryAdminRestControllerSpringTest.java
+++ src/test/java/ru/javawebinar/topjava/web/user/InMemoryAdminRestControllerSpringTest.java
@@ -10,7 +10,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 import ru.javawebinar.topjava.repository.inmemory.InMemoryUserRepository;
 import ru.javawebinar.topjava.util.exception.NotFoundException;
 
-import static ru.javawebinar.topjava.UserTestData.USER_ID;
+import static ru.javawebinar.topjava.service.UserTestData.NOT_FOUND;
+import static ru.javawebinar.topjava.service.UserTestData.USER_ID;
 
 @ContextConfiguration("classpath:spring/spring-app.xml")
 @RunWith(SpringRunner.class)
